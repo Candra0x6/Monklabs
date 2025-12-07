3e67d3f9abab1b2f367863babb0f215ca96e2e34
C N <147255377+Candra0x6@users.noreply.github.com>
2025-12-07 20:52:02 +0700

    feat: add SVG components for ChevronRight, Discord, LineVector, Point, and Twitter

---
+++ src/components/svg/line-vector.tsx
@@ -0,0 +1,10 @@
+
+function LineVector() {
+  return (
+<svg width="224" height="50" viewBox="0 0 224 50" className="translate-y-1/2" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0.381836 49L41.3818 0.5H223.382" stroke="#FF7300"/>
+</svg>
+  )
+}
+
+export default LineVector
