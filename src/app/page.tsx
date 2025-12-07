f13e5ffae84a759c4b1bb1fcbea0cb4fc60bb116
C N <147255377+Candra0x6@users.noreply.github.com>
2025-12-07 20:48:24 +0700

    refactor: clean up imports and streamline component structure in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,22 @@
-import { Footer } from "@/components/Footer";
+import BrowseReflix from "@/components/BrowseReflix";
+import Footer from "@/components/Footer";
 import { Hero } from "@/components/landing-paage/hero";
-import { SectionEmpower } from "@/components/SectionEmpower";
 import { SectionFeatures } from "@/components/SectionFeatures";
 import { SectionFuture } from "@/components/SectionFuture";
 import { SectionLiveRaffles } from "@/components/SectionLiveRaffles";
-import { SectionPartners } from "@/components/SectionPartners";
 import { SectionSellers } from "@/components/SectionSellers";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    
-    <Hero />
-    <SectionFuture />
-    <SectionSellers />
-    <SectionFeatures />
-    <SectionLiveRaffles />
-<section>
-  sknaksn
-</section>
+
+      <Hero />
+      <SectionFuture />
+      <SectionSellers />
+      <SectionFeatures />
+      <SectionLiveRaffles />
+      <BrowseReflix />
+      <Footer />
     </>
   );
 }
