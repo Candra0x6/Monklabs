8b330c9d0ab92bc5bdb98e8dd295dff04f94e720
C N <147255377+Candra0x6@users.noreply.github.com>
2025-12-10 16:12:06 +0700

    feat: update MainFooter styles for improved readability and consistency, adjust hover effects, and ensure proper icon usage

--- src/components/MainFooter.tsx
+++ src/components/MainFooter.tsx
@@ -3,11 +3,12 @@ import { Button } from './ui/button'
 import Point from './svg/point'
 import Discord from './svg/discord'
 import Twitter from './svg/twitter'
+import HelpIcon from './svg/help-icon'
 
 function MainFooter() {
     return (
-        <div className="bg-base-dark-secondary border-t-base-placeholder flex w-full items-center justify-between gap-1 rounded-t-xl border-t py-2 text-xs text-white max-lg:flex-col lg:h-[32px] lg:px-6">
-            <div className="text-primary flex items-center space-x-4">
+        <div className="bg-dark-secondary border-t-base-placeholder flex w-full items-center justify-between gap-1 rounded-t-xl border-t py-2 text-xs text-white max-md:flex-col lg:h-[32px] lg:px-6">
+            <div className="text-primary flex items-start space-x-4">
                 <div className="flex items-center gap-1 space-x-1">
                     <Point />
                     <span>Client Network</span>
@@ -25,10 +26,11 @@ function MainFooter() {
                     <Twitter />
                 </Button>
 
-                <span className='hidden cursor-pointer transition-all duration-300 hover:text-white lg:flex'>
+                <span className='hidden cursor-pointer transition-all duration-300 text-muted lg:flex'>
                     Terms of Service
                 </span>
-                <div className="flex cursor-pointer items-center gap-[8px] transition-all duration-300 hover:text-white">
+                <div className="flex cursor-pointer items-center gap-[8px] transition-all duration-300 text-muted">
+                    <HelpIcon />
                     <span className='lg:flex'>
                         Help and Support
                     </span>
