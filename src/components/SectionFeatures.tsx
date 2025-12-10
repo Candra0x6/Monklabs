4d7de2e91b44519619b45804c61a14c7a4210099
C N <147255377+Candra0x6@users.noreply.github.com>
2025-12-10 15:59:02 +0700

    Refactor SectionFeatures component to use custom hook for marquee animations and extract FeatureItem as a separate component

--- src/components/SectionFeatures.tsx
+++ src/components/SectionFeatures.tsx
@@ -1,48 +1,46 @@
-/* eslint-disable react/jsx-no-comment-textnodes */
 "use client"
-import React, { useEffect, useRef } from 'react';
-import { ArrowUpRight } from 'lucide-react';
-import { FEATURES } from '@/lib/contants/constants';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { Card, CardTitle, CardDescription } from '@/components/ui/card';
-
-gsap.registerPlugin(ScrollTrigger);
+import React, { useRef } from 'react';
+import { useMarqueeAnimations } from '@/hooks/useMarqueeAnimations';
 
 export const SectionFeatures: React.FC = () => {
     const marqueeRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        const ctx = gsap.context(() => {
-            if (marqueeRef.current) {
-                gsap.to(marqueeRef.current, {
-                    xPercent: -50,
-                    repeat: -1,
-                    duration: 20,
-                    ease: "none",
-                });
-            }
-        });
-
-        return () => ctx.revert();
-    }, []);
+    useMarqueeAnimations([marqueeRef], { duration: 20 });
 
     return (
-        <section className='h-[280px] flex items-center justify-center border-b border-base-border-2 overflow-hidden'>
+        <section className='flex h-[280px] items-center justify-center overflow-hidden border-b border-border-secondary bg-background-secondary'>
             <div className="w-full">
                 <div className="flex w-fit gap-[200px]" ref={marqueeRef}>
                     {[1, 2, 3, 4].map((i) => (
-                        <div key={i} className="lg:max-w-[695px] max-w-[380px] w-full max-lg:space-y-4 h-fit relative shrink-0">
-                            <p className='lg:absolute top-4 right-4 lg:max-w-[143px] text-text-tertiary lg:text-right text-sm'>
-                                // EVM COMPATIBLE(BASE)
-                            </p>
-                            <p className='font-semibold uppercase text-pumpkin-100 lg:text-[72px] text-4xl'>
-                                Fair. transparent. fun
-                            </p>
-                        </div>
+                        <>
+                            <FeatureItem key={i} />
+                            <div className="relative h-fit w-full max-w-[380px] shrink-0 max-lg:space-y-4 lg:max-w-[695px]">
+                                <p className='text-sm text-muted lg:absolute lg:right-4  lg:bottom-4 lg:max-w-[143px] lg:text-right'>
+                                    {'// CHAINLINK'} <br /> {'VRF'}
+                                </p>
+                                <p className='text-4xl font-semibold uppercase text-pumpkin-100 lg:text-[72px]'>
+                                    Trusted
+                                    Randomness
+                                </p>
+                            </div>
+                        </>
                     ))}
                 </div>
             </div>
         </section>
     );
 };
+
+const FeatureItem: React.FC = () => (
+
+    <div className="relative h-fit w-full max-w-[380px] shrink-0 max-lg:space-y-4 lg:max-w-[695px]">
+        <p className='text-sm text-muted lg:absolute lg:right-4 lg:top-4 lg:max-w-[143px] lg:text-right'>
+            {'// EVM COMPATIBLE(BASE)'}
+        </p>
+        <p className='text-4xl font-semibold uppercase text-pumpkin-100 lg:text-[72px]'>
+            Fair. transparent. fun
+        </p>
+    </div>
+
+
+);
