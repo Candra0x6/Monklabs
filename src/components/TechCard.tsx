4a33ce61f806b4b3288e96f7c386435facf9e2e3
C N <147255377+Candra0x6@users.noreply.github.com>
2025-12-10 16:14:50 +0700

    feat: refactor TechCard component for improved structure and styling, update background colors and remove default image dimensions

--- src/components/TechCard.tsx
+++ src/components/TechCard.tsx
@@ -9,7 +9,7 @@ interface TechCardProps {
   frontTitle: string;
   frontDescription: string;
   showFrontArrow?: boolean;
-  
+
   // Back face content
   backTopText?: string;
   backBottomText?: string;
@@ -17,17 +17,17 @@ interface TechCardProps {
   backImageWidth?: number;
   backImageHeight?: number;
   backImageOpacity?: number;
-  
+
   // Center image (back face)
   centerImage?: string;
   centerImageWidth?: number;
   centerImageHeight?: number;
-  
+
   // Styling
   width?: string;
   height?: string;
   cornerMarkersVariant?: 'default' | 'inner' | 'outer' | 'top' | 'bottom' | 'wide-top';
-  
+
   // Custom transform on hover
   hoverTranslateX?: number;
   hoverTranslateY?: number;
@@ -41,12 +41,7 @@ const TechCard: React.FC<TechCardProps> = ({
   backTopText = '// NFT TICKETS',
   backBottomText = '// NFT TICKETS',
   backImage,
-  backImageWidth = 400,
-  backImageHeight = 400,
-  backImageOpacity = 0.3,
   centerImage,
-  centerImageWidth = 300,
-  centerImageHeight = 300,
   width = 'w-[379px]',
   height = 'h-[510px]',
   cornerMarkersVariant = 'wide-top',
@@ -54,8 +49,8 @@ const TechCard: React.FC<TechCardProps> = ({
   hoverTranslateY = 0,
   hoverRotateZ = 0,
 }) => {
-    const hoverTransform = `translateX(${hoverTranslateX}px) translateY(${hoverTranslateY}px) rotateZ(${hoverRotateZ}deg)`;
-    const clipPathValue = `polygon(
+  const hoverTransform = `translateX(${hoverTranslateX}px) translateY(${hoverTranslateY}px) rotateZ(${hoverRotateZ}deg)`;
+  const clipPathValue = `polygon(
     2.7rem 0%, 
     96% 0%, 
     96% 40%, 
@@ -67,122 +62,122 @@ const TechCard: React.FC<TechCardProps> = ({
     0% 1.5rem
   )`;
 
-    return (
-        <div className={`group relative ${width} ${height} transform-3d`}
-        >
-            <div className="relative w-full h-full transition-all duration-500 transform-3d">
-              <div style={{ transform: `${hoverTransform}` }} className="absolute inset-0 w-full h-full transition-transform duration-500 transform-3d">
+  return (
+    <div className={`group relative ${width} ${height} transform-3d`}
+    >
+      <div className="relative w-full h-full transition-all duration-500 transform-3d">
+        <div style={{ transform: `${hoverTransform}` }} className="absolute inset-0 w-full h-full transition-transform duration-500 transform-3d">
 
-                {/* Front Face */}
-                <div className="absolute inset-0" style={{ backfaceVisibility: 'hidden', WebkitBackfaceVisibility: 'hidden' }}>
-                  
-                     {/* Border Layer */}
-                    <div
-                        className="absolute inset-0 bg-[#484848]"
-                        style={{ clipPath: clipPathValue, transform: 'scaleX(-1)' }}
-                    />
-                    {/* Main Card Shape Background */}
-                    <div
-                        className="absolute inset-px overflow-hidden bg-background "
-                        style={{ clipPath: clipPathValue, transform: 'scaleX(-1)' }}
-                    />
+          {/* Front Face */}
+          <div className="absolute inset-0 " style={{ backfaceVisibility: 'hidden', WebkitBackfaceVisibility: 'hidden' }}>
 
-                    {/* --- Content Layout --- */}
-                    <div className="absolute inset-px text-primary overflow-hidden">
-                        <div className="relative h-full flex flex-col justify-between p-10 z-10">
+            {/* Border Layer */}
+            <div
+              className="absolute inset-0 bg-border-secondary"
+              style={{ clipPath: clipPathValue, transform: 'scaleX(-1)' }}
+            />
+            {/* Main Card Shape Background */}
+            <div
+              className="absolute inset-px overflow-hidden bg-background "
+              style={{ clipPath: clipPathValue, transform: 'scaleX(-1)' }}
+            />
 
-                            {/* Header Area */}
-                            <div className="flex justify-between items-start relative">
-                                {/* Main Headline */}
-                                <h1 className="font-regular text-[14px] uppercase ">
-                                    {backTopText}
-                                </h1>
-                            </div>
-                            
-                            {/* Center Image */}
-                            {centerImage && (
-                              <div className="w-full h-full relative flex items-center justify-center">
-                                <div className="absolute -right-10 -top-10 ">
-                                    <Image 
-                                      src={centerImage} 
-                                      alt="center" 
-                                      width={400} 
-                                      height={400} 
-                                      loading='lazy' 
-                                      className="object-contain" 
-                                    />
-                                </div>
-                              </div>
-                            )}
+            {/* --- Content Layout --- */}
+            <div className="absolute inset-px text-primary overflow-hidden">
+              <div className="relative h-full flex flex-col justify-between p-10 z-10">
 
-                            <CornerMarkers variant={cornerMarkersVariant} />
-                            
-                            {/* Footer Area */}
-                            <div className="absolute bottom-30 -left-5">
-                                <h1 className="font-regular text-[14px] rotate-[-90deg] uppercase flex flex-col">
-                                    {backBottomText}
-                                </h1>
-                            </div>
-
-                            {/* Back Image */}
-                            {backImage && (
-                              <div className="absolute -right-5 top-20 ">
-                                    <Image 
-                                      src={backImage} 
-                                      alt="center" 
-                                      width={250} 
-                                      height={250} 
-                                      loading='lazy' 
-                                      className="object-cover" 
-                                    />
-                                </div>
-                            )}
-                        </div>
-                    </div> 
+                {/* Header Area */}
+                <div className="flex justify-between items-start relative">
+                  {/* Main Headline */}
+                  <h1 className="font-regular text-[14px] uppercase ">
+                    {backTopText}
+                  </h1>
                 </div>
 
-                {/* Back Face */}
-                <div className="absolute inset-0" style={{ transform: 'rotateY(180deg)', backfaceVisibility: 'hidden', WebkitBackfaceVisibility: 'hidden' }}>
-                {/* Border Layer */}
-                    <div
-                        className="absolute inset-0 bg-[#484848]"
-                        style={{ clipPath: clipPathValue }}
-                    />
-                    {/* Main Card Shape Background */}
-                    <div
-                        className="absolute inset-px text-primary bg-background "
-                        style={{ clipPath: clipPathValue }}
-                    >
-                        {/* --- Content Layout --- */}
-                        <div className="relative h-full flex flex-col justify-between p-8 z-10">
-                            {/* Top Right Arrow Icon */}
-                            {showFrontArrow && (
-                              <div className="absolute top-3 right-7">
-                                <ArrowRight />
-                              </div>
-                            )}
-                            {/* Header Area */}
-                            <div className="flex justify-between items-start relative">
-                                {/* Main Headline */}
-                                <h1 className="font-regular text-[48px] uppercase flex flex-col">
-                                    {frontTitle}
-                                </h1>
-                            </div>
-                            {/* Footer Area */}
-                            <div className="flex justify-end">
-                                <p style={{ fontFamily: 'var(--font-geist)' }} className="w-[185px] text-[12px] text-[#A1A1A1] ">
-                                    {frontDescription}
-                                </p>
-                            </div>
-                        </div>
+                {/* Center Image */}
+                {centerImage && (
+                  <div className="w-full h-full relative flex items-center justify-center">
+                    <div className="absolute -right-10 -top-10 ">
+                      <Image
+                        src={centerImage}
+                        alt="center"
+                        width={400}
+                        height={400}
+                        loading='lazy'
+                        className="object-contain"
+                      />
                     </div>
+                  </div>
+                )}
 
+                <CornerMarkers variant={cornerMarkersVariant} />
+
+                {/* Footer Area */}
+                <div className="absolute bottom-30 -left-5">
+                  <h1 className="font-regular text-[14px] -rotate-90 uppercase flex flex-col">
+                    {backBottomText}
+                  </h1>
                 </div>
 
+                {/* Back Image */}
+                {backImage && (
+                  <div className="absolute -right-5 top-20 ">
+                    <Image
+                      src={backImage}
+                      alt="center"
+                      width={250}
+                      height={250}
+                      loading='lazy'
+                      className="object-cover"
+                    />
+                  </div>
+                )}
               </div>
             </div>
+          </div>
+
+          {/* Back Face */}
+          <div className="absolute inset-0" style={{ transform: 'rotateY(180deg)', backfaceVisibility: 'hidden', WebkitBackfaceVisibility: 'hidden' }}>
+            {/* Border Layer */}
+            <div
+              className="absolute inset-0 bg-border-secondary"
+              style={{ clipPath: clipPathValue }}
+            />
+            {/* Main Card Shape Background */}
+            <div
+              className="absolute inset-px text-primary bg-background "
+              style={{ clipPath: clipPathValue }}
+            >
+              {/* --- Content Layout --- */}
+              <div className="relative h-full flex flex-col justify-between p-8 z-10">
+                {/* Top Right Arrow Icon */}
+                {showFrontArrow && (
+                  <div className="absolute top-3 right-7">
+                    <ArrowRight />
+                  </div>
+                )}
+                {/* Header Area */}
+                <div className="flex justify-between items-start relative">
+                  {/* Main Headline */}
+                  <h1 className="font-regular text-[48px] uppercase flex flex-col">
+                    {frontTitle}
+                  </h1>
+                </div>
+                {/* Footer Area */}
+                <div className="flex justify-end">
+                  <p style={{ fontFamily: 'var(--font-geist)' }} className="w-[185px] text-[12px] text-muted ">
+                    {frontDescription}
+                  </p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default TechCard;
